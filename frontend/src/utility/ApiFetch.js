a0ff724472d4e35f3faa65159bfa23d06c1590e3
connor-jason <connorejason@gmail.com>
2025-10-18 23:12:29 +0000

    jwt name mismatch

--- frontend/src/utility/ApiFetch.js
+++ frontend/src/utility/ApiFetch.js
@@ -1,7 +1,7 @@
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export async function apiFetch(endpoint, options = {}) {
-    const token = localStorage.getItem("jwt");
+    const token = localStorage.getItem("access_token");
 
     const headers = {
         "Content-Type": "application/json",
