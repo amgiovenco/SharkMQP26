4da175835f28a3d5fc5ce2537b3859eadc292dcb
amgiovenco <amgiovenco@wpi.edu>
2026-02-17 03:16:06 +0000

    frontend things

---
+++ frontend/src/components/HomePageOcean.jsx
@@ -0,0 +1,12 @@
+<div style={{ width: '1080px', height: '1080px', position: 'relative' }}>
+  <FloatingLines
+    linesGradient={["#2681f7","#001670","#6f7076"]}
+    animationSpeed={1}
+    interactive={false}
+    bendRadius={1}
+    bendStrength={14.5}
+    mouseDamping={0.07}
+    parallax
+    parallaxStrength={0.2}
+  />
+</div>
